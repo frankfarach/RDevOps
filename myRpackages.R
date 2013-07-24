82ba1dc9798383706131fc5fa6229bfe06486dd7
Frank Farach <frank.farach@gmail.com>
2013-07-23 22:54:55 -0700

    Added script to install common R packages

---
+++ myRpackages.R
@@ -0,0 +1,37 @@
+# Install commonly used packages
+# Useful to run when updating R
+
+install.packages(c("Amelia",
+                   "blockrand",
+                   "ggplot2",
+                   "reshape",
+                   "reshape2",
+                   "Hmisc",
+                   "pander",
+                   "rapport",
+                   "geepack",
+                   "MASS",
+                   "mediation",
+                   "mi",
+                   "psych",
+                   "psychometric",
+                   "plyr",
+                   "WhatIf",
+                   "Zelig",
+                   "ProjectTemplate",
+                   "plink",
+                   "ltm",
+                   "lordif",
+                   "rgm",
+                   "devtools"
+                   )
+               )
+
+# Install Shiny
+options(repos=c(RStudio='http://rstudio.org/_packages', getOption('repos')))
+install.packages('shiny')
+
+# Install Slidify
+library(devtools)
+install_github("slidify", "ramnathv")
+install_github("slidifyLibraries", "ramnathv")
